e23372ef8d27dbcd1622ab51f09c66873a50f854
BertoPolo <bertopolo91@gmail.com>
2022-05-20 16:15:43 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,32 +2,35 @@ import logo from "./logo.svg"
 import "./App.css"
 import { BrowserRouter, Routes, Router } from "react-router-dom"
 import Home from "./components/Home"
-import {  useEffect} from "react";
+import { useEffect, useState } from "react"
 //6d07eb96c68f9d57bc8d4b7a3e1731c8
 
+//https://api.openweathermap.org/data/2.5/weather?q=$%7Bquery%7D&appid=6d07eb96c68f9d57bc8d4b7a3e1731c8
+
 function App() {
+  // useEffect({}, [])
+  const [cityInput, setCityInput] = useState("")
+  const [city, setCity] = useState([])
 
-  useEffect({
-
-  },[])
-
-  const citiesRetriver = async (city) => {
+  const wheatherRetriver = async () => {
     try {
-      https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=6d07eb96c68f9d57bc8d4b7a3e1731c8
-
-      const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=10&appid=6d07eb96c68f9d57bc8d4b7a3e1731c8`)
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=6d07eb96c68f9d57bc8d4b7a3e1731c8`)
       const data = await response.json()
       console.log(data)
+      setCity(data)
     } catch (error) {
       console.log(error)
     }
   }
-
-  const wheatherRetriver
   return (
     <div className="App">
       <header className="App-header">
-        <Home />
+        {/* <Home /> */}
+        <input type="text" onChange={(e) => setCityInput(e.target.value)} />
+        {cityInput && <button onClick={wheatherRetriver}>enter</button>}
+
+        <p>{city.name}</p>
+        <p>{city.weather[0].description}</p>
       </header>
     </div>
   )
